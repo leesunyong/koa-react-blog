ccc9e885af74e73aef29b77c104b1e469291773e
leesunyong95 <55078646+leesunyong95@users.noreply.github.com>
2020-09-08 18:28:07 +0900

    09.08 final

--- client/src/redux/modules/index.js
+++ client/src/redux/modules/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
-import post from './post';
 
 
 import { penderReducer } from 'redux-pender';
 
 
 export default combineReducers({
-    post,
     pender: penderReducer
 });
