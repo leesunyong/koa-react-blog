7f9733f11f5c936bbd491609b0e776d18a7aec7b
leesunyong95 <55078646+leesunyong95@users.noreply.github.com>
2020-09-09 11:31:32 +0900

    clean

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
-import { Provider } from 'react-redux';
 
 class Root extends React.Component {
     
     render (){
         return (
-            <Provider store={this.props.store}>
-                <BrowserRouter>
-                    <Route path="/" component={App}/>
-                </BrowserRouter>
-            </Provider>
+            <BrowserRouter>
+                <Route path="/" component={App}/>
+            </BrowserRouter>
         )
     }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
 import * as serviceWorker from './serviceWorker';
-import configureStore from 'redux/configureStore';
 
-const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root store={store}/>
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
